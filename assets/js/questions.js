f798fda1e0591560fcbe3b014a8d90b9415cb2cd
Shakim Eatmon <83142444+Chubbsdaweb@users.noreply.github.com>
2021-06-21 13:42:25 -0400

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+      title: "Is a variable named 'apple' same as 'Apple' in javascript?",
+      choices: [
+        "Yes",
+        "No",
+        "None of the above",
+        "Only when we use 'strict'"
+        ],
+      answer: "No"
+    },
+    {
+      title: "How many ways are there with which we can declare a variable in javascript?",
+      choices: [
+        "1",
+        "Infinitely many",
+        "3",
+        "None of the above"
+      ],
+      answer: "3"
+    },
+    {
+      title: "Is it necessary for the external script file to contain a <script> tag?",
+      choices: [
+        "Yes",
+        "Depends on the type of include",
+        "No",
+        "None of the above"
+    ],
+      answer: "No"
+    },
+    {
+      title: "Inside which HTML element do we put the JavaScript?",
+      choices: [
+        "<script>",
+        "<js>",
+        "<scr>",
+        "javascript>",
+      ],
+      answer: "<script>"
+    },
+    { title: "How can you add a comment in a JavaScript?",
+    choices: [
+      "//This is a comment",
+      "<!--This is a comment-->",
+      "'This is a comment'",
+      "None of the above",
+    ],
+    answer: "//This is a comment"
+    }
+    
+];
